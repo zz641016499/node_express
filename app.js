e5d3013ac07f4591798d04e50d9ae0fedd2c1918
iron <7821307+ironpetter@user.noreply.gitee.com>
2024-07-03 05:32:39 +0800

    跟换为mongodb

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
-
-const { getDb, saveDb } = require("./db");
 const app = express();
+// const { getDb, saveDb } = require("./db");
+const {
+  getDb,
+  closeConnection,
+  saveOne,
+  delOne,
+  delMany,
+  updateOne,
+} = require("./db.js");
+const uuid = require("uuid");
+
 // app.use(express.urlencoded());
 app.use(express.json());
 // 获取用户列表
 app.get("/", async (req, res) => {
   try {
-    const objs = await getDb();
-    res.send(objs.users);
-  } catch (error) {
+    const result = await getDb();
+    res.send(result);
+  } catch (collection) {
+    console.log("errr");
     res.status(500).json({ error });
   }
 });
@@ -17,18 +27,16 @@ app.get("/", async (req, res) => {
 // 添加用户
 app.post("/", async (req, res) => {
   const body = req.body;
-  if (!body || !body.name.trim()) {
+  body.id = uuid.v4();
+  if (!body || !body.username.trim()) {
     res.status(403).json({
       success: false,
       error: "用户名不能为空",
     });
   }
-  const obj = await getDb();
-  body.id = obj.users.length ? obj.users[obj.users.length - 1].id + 1 : 1001;
-  obj.users.push(body);
   try {
-    const w = await saveDb(obj);
-    if (!w) {
+    const w = await saveOne(body);
+    if (w.acknowledged) {
       res.status(200).send({
         success: true,
         msg: "添加成功！",
@@ -41,25 +49,14 @@ app.post("/", async (req, res) => {
 
 // 修改用户
 app.put("/:id", async (req, res) => {
-  const body = req.body;
+  const { body } = req;
   let { id } = req.params;
-  id = Number(id);
-  const data = await getDb();
-  let index = data.users.findIndex((user) => user.id === id);
-  if (index == -1) {
-    res.status(403).json({
-      success: false,
-      error: "当前用户不存在",
-    });
-  }
-  data.users[index] = { ...body, id };
-
   try {
-    const w = await saveDb(data);
-    if (!w) {
+    const w = await updateOne(id, body);
+    if (w.acknowledged) {
       res.status(200).send({
         success: true,
-        msg: "修改成功！",
+        msg: "删除成功！",
       });
     }
   } catch (error) {
@@ -70,20 +67,9 @@ app.put("/:id", async (req, res) => {
 // 删除用户
 app.delete("/:id", async (req, res) => {
   let { id } = req.params;
-  id = Number(id);
-  const data = await getDb();
-  let users = data.users;
-  const index = users.findIndex((user) => user.id === id);
-  if (index == -1) {
-    res.status(502).json({
-      success: false,
-      error: "用户id" + id + "不存在",
-    });
-  }
-  users.splice(index, 1);
   try {
-    const w = await saveDb(data);
-    if (!w) {
+    const w = await delOne(id);
+    if (w.acknowledged) {
       res.status(200).send({
         success: true,
         msg: "删除成功！",
@@ -98,30 +84,9 @@ app.delete("/:id", async (req, res) => {
 app.post("/batch", async (req, res) => {
   const body = req.body;
   const { ids } = body;
-  if (!ids || !Array.isArray(ids) || !ids.length) {
-    res.status(403).json({
-      success: false,
-      error: "用户id不能为空",
-    });
-  }
-  const data = await getDb();
-  let users = data.users;
-  // 这里需要做一步数据回滚
-  for (let i = 0; i < ids.length; i++) {
-    const id = ids[i];
-    const index = users.findIndex((user) => user.id === id);
-    if (index === -1) {
-      res.status(403).json({
-        success: false,
-        error: "用户id" + id + "不存在",
-      });
-      return; // 立即终止函数执行
-    }
-    users.splice(index, 1);
-  }
   try {
-    const w = await saveDb(data);
-    if (!w) {
+    const w = await delMany(ids);
+    if (w.acknowledged) {
       res.status(200).send({
         success: true,
         msg: "删除成功！",
@@ -134,4 +99,10 @@ app.post("/batch", async (req, res) => {
 
 app.listen(3000, () => {
   console.log("express 3000 ok");
+});
+
+// 在应用程序关闭时关闭连接
+process.on("SIGINT", async () => {
+  await closeConnection();
+  process.exit(0);
 });
--- db.js
+++ db.js
@@ -1,15 +1,68 @@
-const fs = require("fs");
-// promise 语法糖支持
-const { promisify } = require("util");
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
+const { MongoClient, ObjectId } = require("mongodb");
 
-exports.getDb = async () => {
-  let data = await readFile("./db.json", "utf8");
-  return JSON.parse(data);
+const url = "mongodb://localhost:27017";
+const dbName = "mytable";
+const client = new MongoClient(url);
+
+const connectToDatabase = async (name) => {
+  await client.connect();
+  const db = client.db(dbName);
+  const collections = await db.listCollections({ name }).toArray();
+
+  // 如果集合不存在，则创建集合
+  if (collections.length === 0) {
+    await db.createCollection(name);
+  }
+
+  return db.collection(name);
 };
 
-exports.saveDb = async (data) => {
-  const sdata = JSON.stringify(data);
-  return await writeFile("./db.json", sdata);
+const closeConnection = async () => {
+  if (client && client.topology.isConnected()) {
+    await client.close();
+    console.log("Connection closed");
+  }
+};
+
+const getDb = async (name = "user") => {
+  const user = await connectToDatabase(name);
+  const result = await user.find().toArray();
+  return result;
+};
+const getOne = async (options = { name: "user" }) => {
+  const { name, _id } = options;
+  const user = await connectToDatabase(name);
+  const result = await user.find({ _id }).toArray();
+  return result;
+};
+const saveOne = async (data, name = "user") => {
+  const user = await connectToDatabase(name);
+  const result = await user.insertOne(data);
+  return result;
+};
+const updateOne = async (id, data, name = "user") => {
+  const user = await connectToDatabase(name);
+  const result = await user.updateOne({ id }, { $set: data });
+  return result;
+};
+const delOne = async (id, name = "user") => {
+  const user = await connectToDatabase(name);
+  const result = await user.deleteOne({ id });
+  return result;
+};
+const delMany = async (ids, name = "user") => {
+  const user = await connectToDatabase(name);
+  const result = await user.deleteMany({ id: { $in: ids } });
+  return result;
+};
+
+module.exports = {
+  connectToDatabase,
+  closeConnection,
+  getDb,
+  getOne,
+  saveOne,
+  delOne,
+  delMany,
+  updateOne,
 };
